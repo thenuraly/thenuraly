931194340696a893679d548f0fc3232de6c5774b
Nuraly Yessengeldi <nuralyesengeldi49@gmail.com>
2024-11-20 17:24:20 +0200

    Completion_of_lab9

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,3 +227,11 @@
     document.addEventListener('scroll', navmenuScrollspy);
   
   })();
+
+  const toggleSwitch = document.getElementById('modeToggle');
+
+// Add event listener for toggle switch
+toggleSwitch.addEventListener('change', function () {
+    // Toggle the 'dark-mode' class on the body
+    document.body.classList.toggle('dark-mode');
+});
